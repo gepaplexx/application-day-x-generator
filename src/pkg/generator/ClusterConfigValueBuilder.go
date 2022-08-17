9b920582ccf65d076052a14aae5e40b5158ef21a
Herbert Wirnsberger <herbert.wirnsberger@gepardec.com>
2022-08-17 16:33:29 +0200

    GPX-432 - cleanup slack-hooks secrets für alertmanagerconfig

--- src/pkg/generator/ClusterConfigValueBuilder.go
+++ src/pkg/generator/ClusterConfigValueBuilder.go
@@ -19,57 +19,20 @@ data:
   sshPrivateKey: "{{ .ArgocdWorkflowrepositorySshPrivateKey }}"
 `
 
-const ALERTS_SLACK_HOOK_SECRET_KUBE_TEMPLATE string = `
-kind: Secret
-apiVersion: v1
-metadata:
-  name: slack-hook
-  namespace: openshift-kube-controller-manager-operator
-data:
-  url: >-
-    {{ .AlertsSlackUrl }}
-`
-
-const ALERTS_SLACK_HOOK_SECRET_APISERVER_TEMPLATE string = `
-kind: Secret
-apiVersion: v1
-metadata:
-  name: slack-hook
-  namespace: openshift-apiserver
-data:
-  url: >-
-    {{ .AlertsSlackUrl }}
-`
-
 func (gen *ClusterConfigValueBuilder) GetValues(config map[string]Value) (map[string]Value, error) {
-    values := make(map[string]Value)
+	values := make(map[string]Value)
 
-    secretValsArgo := make(map[string]string, 3)
-    secretValsArgo["ArgocdWorkflowrepositoryUsername"] = utils.Base64(config["ArgocdWorkflowrepositoryUsername"])
-    secretValsArgo["ArgocdWorkflowrepositorySshPrivateKey"] = config["ArgocdWorkflowrepositorySshPrivateKeyBase64"].String()
-    encryptedValues, err := encrypt(WORKFLOW_REPO_SECRET_TEMPLATE, secretValsArgo, config["env"], "username", "sshPrivateKey")
-    if err != nil {
-        return nil, err
-    }
-    values["ArgocdWorkflowrepositoryUsername"] = encryptedValues["username"]
-    values["ArgocdWorkflowrepositorySshPrivateKey"] = encryptedValues["sshPrivateKey"]
+	secretValsArgo := make(map[string]string, 3)
+	secretValsArgo["ArgocdWorkflowrepositoryUsername"] = utils.Base64(config["ArgocdWorkflowrepositoryUsername"])
+	secretValsArgo["ArgocdWorkflowrepositorySshPrivateKey"] = config["ArgocdWorkflowrepositorySshPrivateKeyBase64"].String()
+	encryptedValues, err := encrypt(WORKFLOW_REPO_SECRET_TEMPLATE, secretValsArgo, config["env"], "username", "sshPrivateKey")
+	if err != nil {
+		return nil, err
+	}
+	values["ArgocdWorkflowrepositoryUsername"] = encryptedValues["username"]
+	values["ArgocdWorkflowrepositorySshPrivateKey"] = encryptedValues["sshPrivateKey"]
 
-    secretValsAlert := make(map[string]string, 1)
-    secretValsAlert["AlertsSlackUrl"] = utils.Base64(config["AlertsSlackUrl"])
-
-    encryptedValues, err = encrypt(ALERTS_SLACK_HOOK_SECRET_KUBE_TEMPLATE, secretValsAlert, config["env"], "url")
-    if err != nil {
-        return nil, err
-    }
-    values["AlertsSlackUrlKube"] = encryptedValues["url"]
-
-    encryptedValues, err = encrypt(ALERTS_SLACK_HOOK_SECRET_APISERVER_TEMPLATE, secretValsAlert, config["env"], "url")
-    if err != nil {
-        return nil, err
-    }
-    values["AlertsSlackUrlApiServer"] = encryptedValues["url"]
-
-    return values, nil
+	return values, nil
 }
 
 func encrypt(template string, values map[string]string, env Value, keys ...string) (map[string]Value, error) {
--- src/pkg/generator/RookCephInstanceValueBuilder.go
+++ src/pkg/generator/RookCephInstanceValueBuilder.go
@@ -1,41 +1,9 @@
 package generator
 
-import (
-	seal "gepaplexx/day-x-generator/pkg/sealedSecrets"
-	utils "gepaplexx/day-x-generator/pkg/util"
-)
-
 type RookCephInstanceValueBuilder struct{}
-
-const GP_ROOK_CEPH_INSTANCE_SECRET_TEMPLATE string = `
-kind: Secret
-apiVersion: v1
-metadata:
-  name: slack-hook
-  namespace: rook-ceph
-data:
-  url: {{ .SlackChannel }}
-type: Opaque
-
-`
 
 func (gen *RookCephInstanceValueBuilder) GetValues(config map[string]Value) (map[string]Value, error) {
 	values := make(map[string]Value)
 	values["RookCephInstanceEnabled"] = config["RookCephInstanceEnabled"]
-	values["SlackChannel"] = config["SlackChannel"]
-
-	secretVals := make(map[string]string, 1)
-	secretVals["SlackChannel"] = utils.Base64(config["SlackChannel"])
-	secretAsByte, err := utils.ReplaceTemplate(secretVals, GP_ROOK_CEPH_INSTANCE_SECRET_TEMPLATE)
-	if err != nil {
-		return nil, err
-	}
-
-	encryptedValues, err := seal.SealValues(secretAsByte, config["env"], "url")
-	if err != nil {
-		return nil, err
-	}
-	values["SlackChannel"] = encryptedValues["url"]
-
 	return values, nil
 }
