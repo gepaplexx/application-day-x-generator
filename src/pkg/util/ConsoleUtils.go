41b5815076ed4e1e1fc21fe21bc18024354df94a
gattma <marko.gattringer@gepardec.com>
2023-03-15 10:25:03 +0100

    updated go-console lib changes

--- src/pkg/util/ConsoleUtils.go
+++ src/pkg/util/ConsoleUtils.go
@@ -1,9 +1,9 @@
 package util
 
 import (
-	"github.com/DrSmithFr/go-console/pkg/color"
-	"github.com/DrSmithFr/go-console/pkg/formatter"
-	"github.com/DrSmithFr/go-console/pkg/output"
+	"github.com/DrSmithFr/go-console/color"
+	"github.com/DrSmithFr/go-console/formatter"
+	"github.com/DrSmithFr/go-console/output"
 
 	"fmt"
 	"strings"
@@ -13,32 +13,32 @@ import (
 	cursor "atomicgo.dev/cursor"
 )
 
-var FMT_GREENBOLD *formatter.OutputFormatterStyle = formatter.NewOutputFormatterStyle(color.GREEN, color.NULL, []string{color.BOLD})
-var FMT_REDBOLD *formatter.OutputFormatterStyle = formatter.NewOutputFormatterStyle(color.RED, color.NULL, []string{color.BOLD})
-var FMT_CYANBOLD *formatter.OutputFormatterStyle = formatter.NewOutputFormatterStyle(color.CYAN, color.NULL, []string{color.BOLD})
+var FMT_GREENBOLD *formatter.OutputFormatterStyle = formatter.NewOutputFormatterStyle(color.Green, color.Null, []string{color.Bold})
+var FMT_REDBOLD *formatter.OutputFormatterStyle = formatter.NewOutputFormatterStyle(color.Red, color.Null, []string{color.Bold})
+var FMT_CYANBOLD *formatter.OutputFormatterStyle = formatter.NewOutputFormatterStyle(color.Cyan, color.Null, []string{color.Bold})
 
 func PrintDescription(conf string) {
-	out := output.NewConsoleOutput(true, nil)
+	out := output.NewCliOutput(true, nil)
 	str := strings.Repeat("#", 80)
-	out.Writeln(str)
-	out.Writeln("# <b>Name:</b><comment> Day-X-Operations Generator</comment>")
-	out.Writeln("# <b>Description:</b><comment> TODO</comment>")
-	out.Writeln("# <b>Author:</b><comment> gattma,fhochleitner</comment>")
-	out.Writeln("# <b>Version:</b><comment> v1.0</comment>")
-	out.Writeln("# <b>Documentation:</b><comment> https://gepardec.atlassian.net/wiki/spaces/G/pages/2393276417/Day-2-Operations</comment>")
+	out.Println(str)
+	out.Println("# <b>Name:</b><comment> Day-X-Operations Generator</comment>")
+	out.Println("# <b>Description:</b><comment> TODO</comment>")
+	out.Println("# <b>Author:</b><comment> gattma,fhochleitner</comment>")
+	out.Println("# <b>Version:</b><comment> v1.0</comment>")
+	out.Println("# <b>Documentation:</b><comment> https://gepardec.atlassian.net/wiki/spaces/G/pages/2393276417/Day-2-Operations</comment>")
 	configLine := fmt.Sprintf("# <b>Configuration:</b><comment> %s</comment>", conf)
-	out.Writeln(configLine)
-	out.Writeln(str)
+	out.Println(configLine)
+	out.Println(str)
 }
 
 func PrintActionHeader(title string) {
 	fmt.Println()
-	out := output.NewConsoleOutput(true, nil)
+	out := output.NewCliOutput(true, nil)
 	n := LINELENGTH - len(title)
 	div := n / 2
-	out.Writeln("<fg=yellow>" + strings.Repeat("#", LINELENGTH) + "</>")
-	out.Writeln(strings.Repeat(" ", div) + title)
-	out.Writeln("<fg=yellow>" + strings.Repeat("#", LINELENGTH) + "</>")
+	out.Println("<fg=yellow>" + strings.Repeat("#", LINELENGTH) + "</>")
+	out.Println(strings.Repeat(" ", div) + title)
+	out.Println("<fg=yellow>" + strings.Repeat("#", LINELENGTH) + "</>")
 }
 
 func PrintAction(action string) {
