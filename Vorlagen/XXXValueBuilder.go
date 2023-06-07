d0111cc3ff4b0822e5c79406fb63cba44b575a32
gattma <marko.gattringer@gepardec.com>
2023-06-07 07:39:07 +0200

    updated config

--- Vorlagen/XXXValueBuilder.go
+++ Vorlagen/XXXValueBuilder.go
@@ -1,15 +1,11 @@
 package generator
 
-import (
-	"gepaplexx/day-x-generator/pkg/util"
-)
-
-type Value = util.Value // TODO: REMOVE ME (im package generator bereits vorhanden)
+import "gepaplexx/day-x-generator/pkg/util"
 
 type XXXValueBuilder struct{}
 
-func (gen *XXXValueBuilder) GetValues(config map[string]Value) (map[string]Value, error) {
-	values := make(map[string]Value)
+func (gen *XXXValueBuilder) GetValues(config map[string]util.Value) (map[string]util.Value, error) {
+	values := make(map[string]util.Value)
 
 	// TODO
 
